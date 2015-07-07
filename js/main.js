2f6e863d472c5c04c2cba530333e7416bf5a34e5
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-07-07 08:40:04 +0300

    Update 2015-07-07T05:40:02.327Z

--- js/main.js
+++ js/main.js
@@ -16750,5 +16750,18 @@ $(document).ready(function() {
             $('main').removeClass('hidden');
         }
     });
+    function indexHeader(){
+        var sectionFirst = $('section.section').first();
+    
+        if (sectionFirst.hasClass("active")) {
+            $('.header').addClass('hidden');
+        } else $('.header').removeClass('hidden');
+    
+        setTimeout(indexHeader, 200);
+    }
+    
+    if ($(".header-index").length) {
+        indexHeader();
+    }
 
 });
