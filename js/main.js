3944528120fdcc3be092d3a5e0d8fc3f10bfb01f
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-07-07 09:07:54 +0300

    Update 2015-07-07T06:07:52.762Z

--- js/main.js
+++ js/main.js
@@ -16666,49 +16666,49 @@ $(document).ready(function() {
         slideSelector: '.slide',
     });
     
-    $('#index-fullpage').fullpage({
-        //Navigation
-        //menu: '#we-menu',
-        lockAnchors: false,
-        anchors:['1', '2', '3', '4', '5', '6'],
-        //slidesNavigation: true,
-        //slidesNavPosition: 'bottom',
-    
-        //Scrolling
-        css3: true,
-        scrollingSpeed: 700,
-        autoScrolling: true,
-        fitToSection: false,
-        scrollBar: false,
-        easing: 'easeInOutCubic',
-        easingcss3: 'ease',
-        loopBottom: false,
-        loopTop: false,
-        loopHorizontal: true,
-        continuousVertical: false,
-        scrollOverflow: true,
-        touchSensitivity: 15,
-        normalScrollElementTouchThreshold: 5,
-        //normalScrollElements: '.vacancy-modal',
-    
-        //Accessibility
-        keyboardScrolling: true,
-        animateAnchor: true,
-        recordHistory: false,
-    
-        //Design
-        //controlArrows: true,
-        //verticalCentered: false,
-        resize : false,
-        responsiveWidth: 0,
-        responsiveHeight: 0,
-        paddingTop: '75px',
-        //paddingBottom: '70px',
-    
-        //Custom selectors
-        sectionSelector: '.section',
-        slideSelector: '.slide',
-    });
+    //$('#index-fullpage').fullpage({
+    //    //Navigation
+    //    //menu: '#we-menu',
+    //    lockAnchors: false,
+    //    anchors:['1', '2', '3', '4', '5', '6'],
+    //    //slidesNavigation: true,
+    //    //slidesNavPosition: 'bottom',
+    //
+    //    //Scrolling
+    //    css3: true,
+    //    scrollingSpeed: 700,
+    //    autoScrolling: true,
+    //    fitToSection: false,
+    //    scrollBar: false,
+    //    easing: 'easeInOutCubic',
+    //    easingcss3: 'ease',
+    //    loopBottom: false,
+    //    loopTop: false,
+    //    loopHorizontal: true,
+    //    continuousVertical: false,
+    //    scrollOverflow: true,
+    //    touchSensitivity: 15,
+    //    normalScrollElementTouchThreshold: 5,
+    //    //normalScrollElements: '.vacancy-modal',
+    //
+    //    //Accessibility
+    //    keyboardScrolling: true,
+    //    animateAnchor: true,
+    //    recordHistory: false,
+    //
+    //    //Design
+    //    //controlArrows: true,
+    //    //verticalCentered: false,
+    //    resize : false,
+    //    responsiveWidth: 0,
+    //    responsiveHeight: 0,
+    //    paddingTop: '75px',
+    //    //paddingBottom: '70px',
+    //
+    //    //Custom selectors
+    //    sectionSelector: '.section',
+    //    slideSelector: '.slide',
+    //});
     function footerPosition(){
         var sectionFirst = $('section.section').first();
     
