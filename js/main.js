f4d962e839d7c9fa4fed7004d79f7d80d5ed9384
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-07-11 09:42:01 +0300

    Update 2015-07-11T06:41:59.115Z

--- js/main.js
+++ js/main.js
@@ -19149,5 +19149,28 @@ $(document).ready(function() {
     });
     
     //sl_number = $('.radio-lbl').index($('.ui-state-active'));
+    $('.js-why-us__title-1').click(function () {
+        thisBlock = $('#why-us-1');
+        allBlocks = $('.js-why-us');
+    
+        allBlocks.addClass('hidden');
+        thisBlock.removeClass('hidden');
+    });
+    
+    $('.js-why-us__title-2').click(function () {
+        thisBlock = $('#why-us-2');
+        allBlocks = $('.js-why-us');
+    
+        allBlocks.addClass('hidden');
+        thisBlock.removeClass('hidden');
+    });
+    
+    $('.js-why-us__title-3').click(function () {
+        thisBlock = $('#why-us-3');
+        allBlocks = $('.js-why-us');
+    
+        allBlocks.addClass('hidden');
+        thisBlock.removeClass('hidden');
+    });
 
 });
