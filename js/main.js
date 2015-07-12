61d1417ab9424ea95f29cadbca2c2e6840aedcdd
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-07-12 06:43:46 +0300

    Update 2015-07-12T03:43:44.232Z

--- js/main.js
+++ js/main.js
@@ -19001,12 +19001,57 @@ $(document).ready(function() {
     }
     
     
-    if ($('#fullpage').length){
-        $('#fullpage').fullpage({
+    if ($('#we-fullpage').length){
+        $('#we-fullpage').fullpage({
             //Navigation
             menu: '#we-menu',
             lockAnchors: false,
             anchors:['we', 'team', 'graduates', 'vacancies'],
+            slidesNavigation: true,
+            slidesNavPosition: 'bottom',
+    
+            //Scrolling
+            css3: true,
+            scrollingSpeed: 700,
+            autoScrolling: true,
+            fitToSection: false,
+            scrollBar: false,
+            easing: 'easeInOutCubic',
+            easingcss3: 'ease',
+            loopBottom: false,
+            loopTop: false,
+            loopHorizontal: true,
+            continuousVertical: false,
+            scrollOverflow: true,
+            touchSensitivity: 15,
+            normalScrollElementTouchThreshold: 5,
+            normalScrollElements: '.vacancy-modal',
+    
+            //Accessibility
+            keyboardScrolling: true,
+            animateAnchor: true,
+            recordHistory: false,
+    
+            //Design
+            controlArrows: true,
+            verticalCentered: false,
+            resize : false,
+            responsiveWidth: 0,
+            responsiveHeight: 0,
+            paddingTop: '75px',
+    
+            //Custom selectors
+            sectionSelector: '.section',
+            slideSelector: '.slide',
+        });
+    }
+    
+    if ($('#employee-fullpage').length){
+        $('#employee-fullpage').fullpage({
+            //Navigation
+            menu: '#we-menu',
+            lockAnchors: false,
+            anchors:['person', 'interview', 'public', 'articles'],
             slidesNavigation: true,
             slidesNavPosition: 'bottom',
     
