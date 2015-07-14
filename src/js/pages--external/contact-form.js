9f51094c685ba95cfb79d0ba3ab7d3eb742a5bd9
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-07-14 13:31:44 +0300

    G11

--- src/js/pages--external/contact-form.js
+++ src/js/pages--external/contact-form.js
@@ -6,13 +6,13 @@ $(".contact-form__textarea").focus(function() {
     $(this).parents(".contact-form__label").children('.js-label').removeClass('invisible');
 });
 
-$(".contact-form__input").blur(function() {
-    $(this).parents(".contact-form__label").children('.js-label').addClass('invisible');
-});
-
-$(".contact-form__textarea").blur(function() {
-    $(this).parents(".contact-form__label").children('.js-label').addClass('invisible');
-});
+//$(".contact-form__input").blur(function() {
+//    $(this).parents(".contact-form__label").children('.js-label').addClass('invisible');
+//});
+//
+//$(".contact-form__textarea").blur(function() {
+//    $(this).parents(".contact-form__label").children('.js-label').addClass('invisible');
+//});
 
 $('.contact-form__btn').click(function(event){
     event.preventDefault();
