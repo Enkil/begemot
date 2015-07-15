af91aeb6c40d8e5b38d54243370cb2cf53cabed4
Igor [Enkil] Timohin <tia@net-simple.ru>
2015-07-15 18:09:31 +0300

    index slider in who-we-are

--- src/js/pages--external/who-we-are.js
+++ src/js/pages--external/who-we-are.js
@@ -1,12 +1,28 @@
-$('.slick').slick({
-    //variableWidth: true,
-    //adaptiveHeight: true,
-    dots:true,
-    prevArrow: $('.who-we-are-nav__prev'),
-    nextArrow: $('.who-we-are-nav__next'),
+//$('.slick').slick({
+//    //variableWidth: true,
+//    //adaptiveHeight: true,
+//    dots:true,
+//    prevArrow: $('.who-we-are-nav__prev'),
+//    nextArrow: $('.who-we-are-nav__next'),
+//
+//    infinite: true,
+//    speed: 500,
+//    fade: true,
+//    cssEase: 'linear'
+//});
 
-    infinite: true,
+var swiper2 = new Swiper('.swiper-container-2', {
+    nextButton: '.who-we-are-nav__next',
+    prevButton: '.who-we-are-nav__prev',
+    spaceBetween: 0,
+    effect: 'fade',
+    keyboardControl: true,
+    slidesPerView: 1,
+    loop: true,
     speed: 500,
-    fade: true,
-    cssEase: 'linear'
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    paginationBulletRender: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + '</span>';
+    }
 });
